3d3b816e38e97ac63c535e51569015e63d72d42d
Glitch (bomb-shelter-buddies) <none>
2018-07-27 23:34:47 +0000

    Updated crates sync

--- ISSUES.md
+++ ISSUES.md
@@ -3,3 +3,4 @@ ____
 
 1) Shockwave sync may not occur until a minute into the experience. Until then anyone other that the first user in the space may see jittering crates without an actual shockwave being simulated.
 2) Buzzer sounds are out of sync.
+3) Crate rotations aren't synchronizing properly.
--- public/crates.js
+++ public/crates.js
@@ -1,3 +1,4 @@
+window.crates = [];
 let i = setInterval(()=>{
   if(!window.gameHasBegun){
    console.log('Waiting for game to begin.');
@@ -10,25 +11,13 @@ let i = setInterval(()=>{
 var initCrates = ()=>{
  console.log('Initializing crates!');
  window.crates = document.querySelectorAll('.crate');
- //addCylinder('5 4 -5.2','0 0 0');
 }
 
 window.updateCrates = (cratesData)=>{
+  if(window.crates.length === 0) return;
   cratesData.forEach( (cd,index)=>{
-    window.crates[index].pause();
     window.crates[index].setAttribute('position',cd.position);
     window.crates[index].setAttribute('rotation',cd.rotation);
-    window.crates[index].play();
+    window.crates[index].components["dynamic-body"].syncToPhysics();
   });
 }
-
-
-// function addCylinder(p,r,){
-//   let c = document.createElement('a-cylinder');
-//   c.setAttribute('position',p);
-//   c.setAttribute('rotation',r);
-//   c.setAttribute('src','https://cdn.glitch.com/3294c4a3-a3d8-412f-a31e-1e03d1cd1cbd%2Fsad.png?1532449709850');
-//   //c.setAttribute('opacity', 0.7);
-//   c.setAttribute('material','transparent: false; side: double; emissive: #aaf');
-//   window.scene.appendChild(c);
-// }
